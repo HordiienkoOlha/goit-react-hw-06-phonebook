dfd77da22756e9e62fc0320ebf3b84c94f5d65ca
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-07-17 14:02:21 +0300

    filter done

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,32 +1,19 @@
-import { useState } from 'react';
 import { Form } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { addFilter } from 'redux/contacts/contactSlice';
 
 const Filter = () => {
-  const [filter, setFilter] = useState('');
   const dispatch = useDispatch();
-
-  const onChange = event => {
-    setFilter(event.target.value.trim());
-    dispatch(addFilter(filter));
-  };
-
-  const onHandlesubmit = event => {
-    event.preventDefault();
-
-    setFilter('');
-  };
-
   return (
     <>
-      <Form onSubmit={onHandlesubmit}>
+      <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Find contacts by name</Form.Label>
           <Form.Control
             type="text"
+            name="filter"
             placeholder="Search..."
-            onChange={onChange}
+            onChange={event=> dispatch(addFilter(event.target.value.trim()))}
           />
         </Form.Group>
       </Form>
